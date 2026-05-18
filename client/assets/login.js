39419a300b6c559467d656fdcbdea8ccc52fdfe4
ash-g773 <aishwaryageorge@ymail.com>
2026-05-18 16:20:47 +0100

    Add login.js basic functionality - requires api to verify credentials

---
+++ client/assets/login.js
@@ -0,0 +1,27 @@
+document.getElementById("login-form").addEventListener("submit", async (e) => {
+    e.preventDefault()
+
+    const form = new FormData(e.target)
+
+    const options = {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: form.get("username"),
+            password: form.get("password")
+        })
+    }
+
+    const response = await fetch("render_api_link/users/login", options)
+    const data = await response.json()
+
+    if (response.status == 200) {
+        //token stuff - not adding yet
+        window.location.assign("game_page.html")
+    } else {
+        alert("Looks like there was a problem logging in..." + data.error)
+    }
+})
