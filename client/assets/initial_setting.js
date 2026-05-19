61b19bc1f1403a8c77628a0df4c1feadc3e18134
ash-g773 <aishwaryageorge@ymail.com>
2026-05-19 15:20:41 +0100

    Add functionality to load in initial_setting value from db - currently no api links

--- client/assets/initial_setting.js
+++ client/assets/initial_setting.js
@@ -3,3 +3,31 @@ document.getElementById("start").addEventListener("click", (e) => {
 
     window.location.assign("game_page.html")
 })
+
+// update gameContainer to have overlay context info
+//async
+function addContext() {
+    const gameContainer = document.getElementById("gameContainer")
+
+    // scenario_id in this MVP is 1, hardcoded?
+    const scenario_id = 1
+
+    // requires api function to retrieve initial_setting from scenarios table using scenario_id
+    // const initialSetting = await fetch(`render_deployed_api/${scenario_id}`)
+    const initialSetting = "Test string - youre on a platform!"
+
+
+    const initialSettingSection = document.createElement("p")
+    initialSettingSection.id = "initialSetting"
+    initialSettingSection.textContent = initialSetting
+
+    gameContainer.appendChild(initialSettingSection)
+    
+}
+
+addContext()
+
+
+
+
+
