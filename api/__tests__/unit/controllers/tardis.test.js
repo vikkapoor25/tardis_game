b0c9f26eff569f66a18c3129846ba2ab5905ac0f
vikram kapoor <vikkapoor@mail.com>
2026-05-20 13:20:24 +0100

    Unit Test Controllers

--- api/__tests__/unit/controllers/tardis.test.js
+++ api/__tests__/unit/controllers/tardis.test.js
@@ -1,5 +1,7 @@
-const goatsController = require('../../../controllers/goats')
-const Goat = require('../../../models/Goat')
+const explanationsController = require('../../../controllers/explanations')
+const scenarioController = require('../../../controllers/scenarios')
+const Explanation = require('../../../models/Explanation')
+const Scenario = require('../../../models/Scenario')
 
 // Mocking response methods
 const mockSend = jest.fn()
@@ -15,149 +17,59 @@ const mockStatus = jest.fn(() => ({
 
 const mockRes = { status: mockStatus };
 
-
-describe('Goats controller', () => {
+describe('Scenario & Explaination Controller', () => {
   beforeEach(() => jest.clearAllMocks())
 
   afterAll(() => jest.resetAllMocks())
 
-  describe('index', () => {
-    it('should return goats with a status code 200', async () => {
-      const testGoats = ['g1', 'g2']
-      jest.spyOn(Goat, 'getAll').mockResolvedValue(testGoats)
+  describe('indexScenario', () => {
+    it('should return scenario with a status code 200', async () => {
+      const testScenario = ['q1', 'q2']
+      jest.spyOn(Scenario, 'getScenario').mockResolvedValue(testScenario)
 
-      await goatsController.index(null, mockRes)
+      await scenarioController.indexScenario(null, mockRes)
       
-      expect(Goat.getAll).toHaveBeenCalledTimes(1)
+      expect(Scenario.getScenario).toHaveBeenCalledTimes(1)
       expect(mockStatus).toHaveBeenCalledWith(200)
-      expect(mockSend).toHaveBeenCalledWith({ data: testGoats })
+      expect(mockSend).toHaveBeenCalledWith({ data: testScenario })
     })
 
     it('should return an error upon failure', async () => {
-      jest.spyOn(Goat, 'getAll').mockRejectedValue(new Error('Something happened to your db'))
+      jest.spyOn(Scenario, 'getScenario').mockRejectedValue(new Error('Something happened to your db'))
 
-      await goatsController.index(null, mockRes)
+      await scenarioController.indexScenario(null, mockRes)
       
-      expect(Goat.getAll).toHaveBeenCalledTimes(1)
+      expect(Scenario.getScenario).toHaveBeenCalledTimes(1)
       expect(mockStatus).toHaveBeenCalledWith(500)
       expect(mockSend).toHaveBeenCalledWith({ error: 'Something happened to your db' })
     })
   })
 
-  describe ('show', () => {
-    let testGoat, mockReq;
+    describe('indexExplanation', () => {
+    it('should return explanation with a status code 200', async () => {
+      const testExplanations = ['e1', 'e2']
+      jest.spyOn(Explanation, 'getAllExplanations').mockResolvedValue(testExplanations)
 
-    beforeEach(() => {
-      testGoat = { id: 1, name: 'Test goat', age: 22 }
-      mockReq = { params: { id: 1 } }
-    });
-
-    it('should return a goat with a 200 status code', async () => {
-      jest.spyOn(Goat, 'findById').mockResolvedValue(new Goat(testGoat))
-
-      await goatsController.show(mockReq, mockRes);
+      await explanationsController.indexExplanation(null, mockRes)
       
-      expect(Goat.findById).toHaveBeenCalledTimes(1);
-      expect(mockStatus).toHaveBeenCalledWith(200);
-      expect(mockSend).toHaveBeenCalledWith({ data: new Goat(testGoat) })
+      expect(Explanation.getAllExplanations).toHaveBeenCalledTimes(1)
+      expect(mockStatus).toHaveBeenCalledWith(200)
+      expect(mockSend).toHaveBeenCalledWith({ data: testExplanations })
     })
 
-    it('should return an error if the goat is not found', async () => {
-      jest.spyOn(Goat, 'findById').mockRejectedValue(new Error('oh no'))
+    it('should return an error upon failure', async () => {
+      jest.spyOn(Explanation, 'getAllExplanations').mockRejectedValue(new Error('Something happened to your db'))
 
-      await goatsController.show(mockReq, mockRes)
+      await explanationsController.indexExplanation(null, mockRes)
       
-      expect(Goat.findById).toHaveBeenCalledTimes(1)
-      expect(mockStatus).toHaveBeenCalledWith(404)
-      expect(mockSend).toHaveBeenCalledWith({ error: 'oh no' })
+      expect(Explanation.getAllExplanations).toHaveBeenCalledTimes(1)
+      expect(mockStatus).toHaveBeenCalledWith(500)
+      expect(mockSend).toHaveBeenCalledWith({ error: 'Something happened to your db' })
     })
   })
 
-  describe ('create', () => {
-    it('should return a new goat with a 201 status code', async () => {
-      let testGoat = { name: 'Test Goat', age: 2 }
-      const mockReq = { body: testGoat }
-
-      jest.spyOn(Goat, 'create').mockResolvedValue(new Goat(testGoat))
-
-      await goatsController.create(mockReq, mockRes)
-      
-      expect(Goat.create).toHaveBeenCalledTimes(1)
-      expect(mockStatus).toHaveBeenCalledWith(201)
-      expect(mockSend).toHaveBeenCalledWith({ data: new Goat({ ...testGoat }) })
-    })
-
-
-    it('should return an error if creation fails', async () => {
-      let testGoat = { name: 'Test Goat' }
-      const mockReq = { body: testGoat }
-
-      jest.spyOn(Goat, 'create').mockRejectedValue(new Error('oh no'))
-
-      await goatsController.create(mockReq, mockRes)
-      
-      expect(Goat.create).toHaveBeenCalledTimes(1)
-      expect(mockStatus).toHaveBeenCalledWith(400)
-      expect(mockSend).toHaveBeenCalledWith({ error: 'oh no' })
-    })
-  })
-
-  describe ('update', () => {
-    it('should update a goat and return it with a 200 status code', async () => {
-      const existingGoat = { id: 22, name: 'Old goat', age: 22 }
-      const updatedGoat = { ...existingGoat, name: 'Updated Goat', age: 4 };
-      const mockReq = { params: { id: 22 }, body: { name: 'Updated Goat', age: 4 } }
-
-      jest.spyOn(Goat, 'findById').mockResolvedValue(new Goat(existingGoat))
-      jest.spyOn(Goat.prototype, 'update').mockResolvedValue(updatedGoat)
-
-      await goatsController.update(mockReq, mockRes)
-
-      expect(Goat.findById).toHaveBeenCalledWith(22);
-      expect(Goat.prototype.update).toHaveBeenCalledWith({ name: 'Updated Goat', age: 4 });
-      expect(mockStatus).toHaveBeenCalledWith(200);
-      expect(mockSend).toHaveBeenCalledWith({ data: updatedGoat });
-    })
-
-    it('should return an error if the goat is not found', async () => {
-      const mockReq = { params: { id: '49' }, body: {} };
-
-      jest.spyOn(Goat, 'findById').mockRejectedValue(new Error('Goat not found'));
-
-      await goatsController.update(mockReq, mockRes);
-
-      expect(Goat.findById).toHaveBeenCalledWith(49);
-      expect(mockStatus).toHaveBeenCalledWith(400);
-      expect(mockSend).toHaveBeenCalledWith({ error: 'Goat not found' });
-    })
-  })
-
-  describe ('destroy', () => {
-    it('should return a 204 status code on successful deletion', async () => {
-      const testGoat = { id: 1, name: 'Test Goat', age: 22 };
-      const mockReq = { params: { id: '1' } };
-
-      jest.spyOn(Goat, 'findById').mockResolvedValue(new Goat(testGoat));
-      jest.spyOn(Goat.prototype, 'destroy').mockResolvedValue();
-
-      await goatsController.destroy(mockReq, mockRes);
-
-      expect(Goat.findById).toHaveBeenCalledWith(1);
-      expect(Goat.prototype.destroy).toHaveBeenCalledTimes(1);
-      expect(mockStatus).toHaveBeenCalledWith(204);
-      expect(mockEnd).toHaveBeenCalled();
-    });
-
-    it('should return an error if the goat is not found', async () => {
-      const mockReq = { params: { id: '49' } };
-
-      jest.spyOn(Goat, 'findById').mockRejectedValue(new Error('Goat not found'));
-
-      await goatsController.destroy(mockReq, mockRes);
-
-      expect(Goat.findById).toHaveBeenCalledWith(49);
-      expect(mockStatus).toHaveBeenCalledWith(404);
-      expect(mockSend).toHaveBeenCalledWith({ error: 'Goat not found' });
-    });
-  })
 })
+
+
+
+
