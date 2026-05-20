8bfd552b4563bcf16be2c606e34d4df787a34516
ash-g773 <aishwaryageorge@ymail.com>
2026-05-20 10:38:22 +0100

    Update package.json to run coverage report for tests, add index.html tests

--- client/__tests__/layout.test.js
+++ client/__tests__/layout.test.js
@@ -5,8 +5,8 @@ let document;
 
 describe("index.html", () => {
     beforeEach(async () => {
-        dom = await renderDOM("./index.html")
-        // we now have access to a fake document for the rest of the tests
+        dom = await renderDOM("./client/index.html")
+        // we now have access to a fake 'document' for the rest of the tests
         document = await dom.window.document
     })
 
@@ -15,19 +15,66 @@ describe("index.html", () => {
         expect(pageTitle).toBeTruthy
         expect(pageTitle.innerHTML).toBe("Tardis")
     })
+    
+    it('has link to login page', () => {
+        const loginLink = document.getElementById('login')
+        expect(loginLink).toBeTruthy
+        expect(loginLink.innerHTML).toBe("Login")
+        expect(loginLink.href).toContain(`/login.html`)
+    })
+
+    it('has link to register page', () => {
+        const loginLink = document.getElementById('register')
+        expect(loginLink).toBeTruthy
+        expect(loginLink.innerHTML).toBe("Sign up")
+        expect(loginLink.href).toContain(`/register.html`)
+    })
 
 })
 
 describe("initial_setting.html", () => {
+    beforeEach(async () => {
+        dom = await renderDOM("./client/intial_setting.html")
+        // we now have access to a fake 'document' for the rest of the tests
+        document = await dom.window.document
+    })
 
 })
 
 
-describe("login.html", () => {
+// describe("login.html", () => {
+//     beforeEach(async () => {
+//         dom = await renderDOM("./client/login.html")
+//         // we now have access to a fake 'document' for the rest of the tests
+//         document = await dom.window.document
+//     })
 
-})
+// })
 
 
-describe("register.html", () => {
+// describe("register.html", () => {
+//     beforeEach(async () => {
+//         dom = await renderDOM("./client/register.html")
+//         // we now have access to a fake 'document' for the rest of the tests
+//         document = await dom.window.document
+//     })
 
-})
+// })
+
+// describe("game_page.html", () => {
+//     beforeEach(async () => {
+//         dom = await renderDOM("./client/game_page.html")
+//         // we now have access to a fake 'document' for the rest of the tests
+//         document = await dom.window.document
+//     })
+
+// })
+
+// describe("results.html", () => {
+//     beforeEach(async () => {
+//         dom = await renderDOM("./client/results.html")
+//         // we now have access to a fake 'document' for the rest of the tests
+//         document = await dom.window.document
+//     })
+
+// })
