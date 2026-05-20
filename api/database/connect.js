b121f2158daf53f5d448782c3866d54bcf16f09c
vikram kapoor <vikkapoor@mail.com>
2026-05-20 11:07:06 +0100

    install jest & supertest, include DB_TEST_URL in connect.js

--- api/database/connect.js
+++ api/database/connect.js
@@ -2,9 +2,12 @@
 const { Pool } = require("pg");
 
 // Creates database connection pool using DB_URL from .env
+// When Integration testing, uses DB_TEST_URL
 const db = new Pool({
-    connectionString: process.env.DB_URL
-});
+  connectionString: process.env.NODE_ENV === "test"
+    ? process.env.DB_TEST_URL
+    : process.env.DB_URL
+})
 
 // Exports database connection
 module.exports = db;
